389c3da8f6ca6edee42c8ef48ec0579d74d684a5
Tim Behrsin <tim@behrsin.com>
2019-06-11 20:07:11 +0100

    add mocked provider for apollo

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MockedProvider } from 'react-apollo/test-utils';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+    <MockedProvider>
+      <App />
+    </MockedProvider>
+  ), div);
   ReactDOM.unmountComponentAtNode(div);
 });
